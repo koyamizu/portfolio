9f96b26bda21c23d3a03ff5b92212f0ff24146dc
koyamizu <saka16156392@gmail.com>
2025-04-14 11:03:05 +0900

    javaプロジェクト化。jdk21で作ってある。

---
+++ time_recorder_and_shift_management/src/main/java/com/example/demo/TimeRecorderAndShiftManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeRecorderAndShiftManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeRecorderAndShiftManagementApplication.class, args);
+	}
+
+}
