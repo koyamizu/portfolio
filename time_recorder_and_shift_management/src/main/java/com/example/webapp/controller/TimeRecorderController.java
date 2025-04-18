5c12280fd9e897cdf611b0c011859829ea538c5a
koyamizu <saka16156392@gmail.com>
2025-04-18 11:06:53 +0900

    打刻ページへのControllerを作成中。

--- time_recorder_and_shift_management/src/main/java/com/example/webapp/controller/TimeRecorderController.java
+++ time_recorder_and_shift_management/src/main/java/com/example/webapp/controller/TimeRecorderController.java
@@ -1,12 +1,16 @@
 package com.example.webapp.controller;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.webapp.entity.ShiftAndTimestamp;
 import com.example.webapp.service.TimeRecorderService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,20 +21,31 @@ import lombok.RequiredArgsConstructor;
 public class TimeRecorderController {
 
 	private final TimeRecorderService timeRecorderService;
-	LocalDate today=LocalDate.now();
-	
+	LocalDate today = LocalDate.now();
+
 	@GetMapping
 	public String employees(Model model) {
-		model.addAttribute("employees",timeRecorderService.selectEmployeesByDate(today));
-		return "time_recorder/time_recorder";
+		model.addAttribute("todaysEmployees", timeRecorderService.selectEmployeesByDate(today));
+		return "time_recorder/top";
 	}
-	
-//	@GetMapping("/time_recorder/dakoku/{id}")
-//	public String start(@PathVariable String id,Model model,RedirectAttributes attributes) {
-//		ShiftAndTimestamp shiftAndTimestamp=timeRecorderService.selectShiftAndTimestampByEmployeeIdAndDate(id, today);
-//		if(shiftAndTimestamp!=null) {
-//			model.addAttribute("")
-//		}
-//	}
-	
+
+	//	プレースホルダがシフトidになっているver.セキュリティ的にもこっちのほうがいいかも知れない
+	@GetMapping("/time_recorder/{id}")
+	public String start(@PathVariable Integer id,Model model,RedirectAttributes attributes) {
+		List<ShiftAndTimestamp> todayEmployees=timeRecorderService.selectEmployeesByDate(today);
+		ShiftAndTimestamp employee=list.stream().filter(s->s.getId()==id);
+		if(employee!=null) {
+			model.addAttribute("")
+		}
+	}
+
+	//	GetMappingではなくPostMappingで、Viewから渡される従業員IDはbodyに格納する。その場合は、url名とかも変える。
+	//	@GetMapping("/time_recorder/{id}")
+	//	public String start(@PathVariable String id,Model model,RedirectAttributes attributes) {
+	//		ShiftAndTimestamp shiftAndTimestamp=timeRecorderService.selectShiftAndTimestampByEmployeeIdAndDate(id, today);
+	//		if(shiftAndTimestamp!=null) {
+	//			model.addAttribute("")
+	//		}
+	//	}
+
 }
