484424f075d558572b63cfe7aa1b6dc9b709d09f
koyamizu <saka16156392@gmail.com>
2025-05-14 08:47:31 +0900

    セッションに遷移元のパスを保存すればいけそうな気がしてきた。それが難しいならhistory.putState使えばいけるかな？

--- time_recorder_and_shift_management/src/main/java/com/example/webapp/config/SecurityConfig.java
+++ time_recorder_and_shift_management/src/main/java/com/example/webapp/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests(
-						authz -> authz.requestMatchers("/css/**", "/js/**","/login","/","/api/event/all").permitAll()
+						authz -> authz.requestMatchers("/css/**", "/js/**","/","/login","/","/api/event/all").permitAll()
 								.requestMatchers("/employees/**","/time_recorder","/shift/management","/admin").hasAuthority("ADMIN")
 								.anyRequest().authenticated())
 				.formLogin(form -> form.loginPage("/login")
