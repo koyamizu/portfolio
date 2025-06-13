2d73b520ec638c93a689b191977baa85810800df
koyamizu <saka16156392@gmail.com>
2025-06-13 10:56:33 +0900

    ログイン→従業員一覧→箱崎の詳細に飛ぶ　というE2Eテストを作成。久しぶりにHerokuにアップしたアプリをいじってみたところ、5xxエラーが多発。業務ロジックがほぼコントローラークラスにあり、例外処理が皆無に等しいので、業務ロジックをサービスクラスに移しつつ、エラーの特定や、例外処理部分を強化したい。

--- src/test/java/com/example/webapp/e2e_test/EmployeeManagementUiTest.java
+++ src/test/java/com/example/webapp/e2e_test/EmployeeManagementUiTest.java
@@ -1,10 +1,25 @@
 package com.example.webapp.e2e_test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+
+import com.codeborne.selenide.Configuration;
 
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@Sql("../repository/EmployeesManagementMapperTest.sql")
+@WithMockUser(username = "admin", authorities = { "ADMIN", "USER" })
 public class EmployeeManagementUiTest {
 
 	@Value("${local.server.port}")
@@ -12,6 +27,24 @@ public class EmployeeManagementUiTest {
 	
 	@BeforeEach
 	void setup() {
+		Configuration.browser="chrome";
 		Configuration.baseUrl="http://localhost:"+randomPort;
+		DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+		String dateTime=LocalDateTime.now().format(formatter);
+		Configuration.reportsFolder="build/reports/screenshots/EmployeeManagementTest/"+dateTime;
+	}
+	
+	@Test
+	void test() {
+		open("/employees");
+		screenshot("LoginPage");
+		$("input[name=employeeIdInput]").setValue("1001");
+		$("input[name=passwordInput]").setValue("yoshizuka01");
+		$("form").submit();
+		assertEquals("従業員管理",title());
+		screenshot("EmployeesListPage");
+		$$("a[href*='/employees/detail/']").last().click();
+		screenshot("DetailHakozaki");
+		$$("table tr td").get(1).should(exactText("箱崎"));
 	}
 }
